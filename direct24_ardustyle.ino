01b5c85f78f2e4f8336a98fb8e23f0b2d5eca455
Nishank Kuppa <nishankkuppa@users.noreply.github.com>
2022-10-10 13:25:31 -0700

    Add files via upload

---
+++ direct24_ardustyle.ino
@@ -0,0 +1,80 @@
+/*
+   Digital I/O (dual 7-segment display board) lab sketch.
+
+   This sketch demonstrates the use of a 7-segment display using direct-access to
+   each of the digit's segments.  The function of this program is left as an exercise
+   to the lab student.
+
+   The arrangement of the digit's LED segments "a" through "g" are documented in the
+   datasheet for the display.  This display is a common-anode type unit.
+
+   v1.00 Eric B. Wertz 2011/09/19 00:03 - Initial revision
+*/
+
+// Arduino digital pin numbers for right-digit direct segment access
+#define RDIGIT_DIRECT_A  8
+#define RDIGIT_DIRECT_B  7
+#define RDIGIT_DIRECT_C  6
+#define RDIGIT_DIRECT_D  5
+#define RDIGIT_DIRECT_E  4
+#define RDIGIT_DIRECT_F  3
+#define RDIGIT_DIRECT_G  2
+
+// min/max pin numbers for looping through all the digit's pins (assumes contiguous pin numbers!)
+#define RDIGIT_DIRECT_MIN 2
+#define RDIGIT_DIRECT_MAX 8
+
+#define DELAY_BLINK_MSECS 500 // milliseconds between blinks
+
+// turn off the display
+void rightDigitOff()
+{
+  byte pin;
+
+  for (pin = RDIGIT_DIRECT_MIN; pin <= RDIGIT_DIRECT_MAX; pin++) {
+    digitalWrite(pin, HIGH);
+  }
+}
+
+void setup()
+{
+  byte pin;
+  // set output pins
+  for (pin = RDIGIT_DIRECT_MIN; pin <= RDIGIT_DIRECT_MAX; pin++) {
+    pinMode(pin, OUTPUT);
+  }
+}
+
+void loop()
+{
+  rightDigitOff();
+
+  delay(DELAY_BLINK_MSECS);
+
+  // display the number 2
+  digitalWrite(RDIGIT_DIRECT_A, LOW);
+  digitalWrite(RDIGIT_DIRECT_B, LOW);
+  digitalWrite(RDIGIT_DIRECT_C, HIGH);
+  digitalWrite(RDIGIT_DIRECT_D, LOW);
+  digitalWrite(RDIGIT_DIRECT_E, LOW);
+  digitalWrite(RDIGIT_DIRECT_F, HIGH);
+  digitalWrite(RDIGIT_DIRECT_G, LOW);
+
+  delay(DELAY_BLINK_MSECS);
+
+  rightDigitOff();
+
+  delay(DELAY_BLINK_MSECS);
+  
+  // display the number 4
+  digitalWrite(RDIGIT_DIRECT_A, HIGH);
+  digitalWrite(RDIGIT_DIRECT_B, LOW);
+  digitalWrite(RDIGIT_DIRECT_C, LOW);
+  digitalWrite(RDIGIT_DIRECT_D, HIGH);
+  digitalWrite(RDIGIT_DIRECT_E, HIGH);
+  digitalWrite(RDIGIT_DIRECT_F, LOW);
+  digitalWrite(RDIGIT_DIRECT_G, LOW);
+
+  delay(DELAY_BLINK_MSECS);
+}
+
---
+++ question5.ino
@@ -0,0 +1,106 @@
+/*
+   Digital I/O (dual 7-segment display board) lab sketch.
+
+   This sketch displays the number "5" on the right-hand digit on the
+   display board using only PORT-style programming.
+
+   Segment "a" is connected to PORTB:0, and "b" through "g" are connected to
+   PORTD:7 through PORTD:2, respectively.  We are avoiding using PORTD:1-0
+   because the serial port is connected to them, and using them can prevent
+   the Serial Console or code download from working.
+
+   Remember that the display is a common-anode type unit, so each segment is active-low.
+
+        PORTB    PORTD      <- PORT
+      --543210  76543210    <- bit number
+      -------a  bcdefg--    <- segment letter
+
+   v1.00 <your name here> <current time/date here>
+*/
+
+int i;  // counter for number displayed
+
+void setup()
+{
+  DDRB = 0b00000001;
+  DDRD = 0b11111111;
+}
+
+void loop()
+{
+  //PORTB = 0b11111110;
+  //PORTD = 0b10010011;
+  for (i = 9; i >= 0 ; i--)
+  {
+    rightDigitDirect(i);
+  }
+}
+
+void rightDigitDirect (int)
+{
+  // each case number corresponds to the number displayed on the 7-segment display board
+  switch (i)
+  {
+    case 9:
+      PORTB = 11111110;
+      PORTD = 00010011;
+      delay(1000);
+      break;
+
+    case 8:
+      PORTB = 11111110;
+      PORTD = 00000011;
+      delay(1000);
+      break;
+
+    case 7:
+      PORTB = 11111110;
+      PORTD = 00111111;
+      delay(1000);
+      break;
+
+    case 6:
+      PORTB = 11111110;
+      PORTD = 10000011;
+      delay(1000);
+      break;
+
+    case 5:
+      PORTB = 11111110;
+      PORTD = 10010011;
+      delay(1000);
+      break;
+
+    case 4:
+      PORTB = 11111111;
+      PORTD = 00110011;
+      delay(1000);
+      break;
+
+    case 3:
+      PORTB = 11111110;
+      PORTD = 00011011;
+      delay(1000);
+      break;
+
+    case 2:
+      PORTB = 11111110;
+      PORTD = 01001011;
+      delay(1000);
+      break;
+
+    case 1:
+      PORTB = 11111111;
+      PORTD = 00111111;
+      delay(1000);
+      break;
+
+    case 0:
+      PORTB = 11111110;
+      PORTD = 00000111;
+      delay(1000);
+      break;
+  }
+}
+
+
